cc62b2587ca9c5c8ae80ca4b19b659de6ecd32ce
cws0316 <dev.chwonseok@gmail.com>
2021-10-08 16:43:13 +0900

    Added get method on express()

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 const PORT = 4000;
 const app = express();
 
+app.get('/', () => console.log('going to home'));
+
 const listeningHandler = () =>
   console.log(`server listening on http://localhost:${PORT}`);
 
