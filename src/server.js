dfa293fbd6904fad5a479260aa9fd12fdc189e51
cws0316 <dev.chwonseok@gmail.com>
2021-10-08 16:29:32 +0900

    Added server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-console.log('hey');
+import express from 'express';
+
+const PORT = 4000;
+const app = express();
+
+const listeningHandler = () =>
+  console.log(`server listening on http://localhost:${PORT}`);
+
+app.listen(PORT, listeningHandler);
