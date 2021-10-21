ea5b7818c553937cd6ff362b66ef26a20439ef20
cws0316 <dev.chwonseok@gmail.com>
2021-10-22 00:49:13 +0900

    Add: regex in videoRouter

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,8 @@
 export const trending = (req, res) => res.send('Trending Videos (Home)');
 export const search = (req, res) => res.send('Search Videos');
-export const see = (req, res) => res.send('See Videos');
+export const see = (req, res) => {
+  return res.send(`See Videos #${req.params.id}`);
+};
 export const edit = (req, res) => res.send('Edit Videos');
 export const remove = (req, res) => res.send('Remove Videos');
 export const upload = (req, res) => res.send('Upload Videos');
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import { edit, see, remove, upload } from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get('/:id', see);
-videoRouter.get('/:id/edit', edit);
-videoRouter.get('/:id/remove', remove);
-videoRouter.get('/upload', upload);
+videoRouter.get('/:id(\\d+)', see);
+videoRouter.get('/:id(\\d+)/edit', edit);
+videoRouter.get('/:id(\\d+)/remove', remove);
+videoRouter.get('/upload', upload); // 위치가 '/:id'보다 위에 있어야 함
 
 export default videoRouter;
